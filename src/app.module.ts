32b215bf784a72c477a48aa7f5cc209343311c00
aamanh30 <aamanh30@gmail.com>
2022-12-18 19:20:26 +0530

    Added the `ServeStaticModule` to serve static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,9 +19,15 @@ import { AssessmentModule } from './modules/assessment/assessment.module';
 import { ObjectiveModule } from './modules/objective/objective.module';
 import { SubjectiveModule } from './modules/subjective/subjective.module';
 import { PlanOfTreatmentModule } from './modules/plan-of-treatment/plan-of-treatment.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/physiodocs-service*']
+    }),
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       useFactory: () => getDatabaseOptions(__dirname)
